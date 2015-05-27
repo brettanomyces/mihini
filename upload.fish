5cfc2bb77a34834b58ee77eccf2019753e295125
yukichb <brett.yukich@fostermoore.com>
2015-05-28 08:36:05 +1200

    create upload file

---
+++ upload.fish
@@ -0,0 +1,20 @@
+
+# due to a bug in Arduino we need to use full paths, see https://github.com/arduino/Arduino/issues/1493
+if test (uname) = 'Linux'
+  set ARDUINO_EXE_PATH /usr/local/bin/arduino
+  set BUILD_PATH ~/git/mihini/build
+  set SKETCHBOOK_PATH ~/git/mihini
+  set INO_PATH ~/git/mihini/mihini/mihini.ino
+else  # Darwin
+  set ARDUINO_EXE_PATH /Applications/Arduino.app/Contents/MacOS/Arduino
+  set BUILD_PATH ~/workspace/mihini/build
+  set SKETCHBOOK_PATH ~/workspace/mihini/
+  set INO_PATH ~/workspace/mihini/mihini/mihini.ino
+end
+
+eval $ARDUINO_EXE_PATH --upload --verbose $INO_PATH \
+  --pref build.path=$BUILD_PATH \
+  --pref sketchbook.path=$SKETCHBOOK_PATH \
+  --board arduino:avr:uno \
+  --port /dev/ttyACM0
+
