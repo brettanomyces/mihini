548cd697a7b08d29e3e2f0d19c4f5704200665a5
yukichb <brett.yukich@fostermoore.com>
2015-05-28 08:35:50 +1200

    add linux options to build file

--- build.fish
+++ build.fish
@@ -1,10 +1,16 @@
-#!/usr/local/bin/fish
 
 # due to a bug in Arduino we need to use full paths, see https://github.com/arduino/Arduino/issues/1493
-set ARDUINO_EXE_PATH /Applications/Arduino.app/Contents/MacOS/Arduino
-set BUILD_PATH ~/workspace/mihini/build
-set SKETCHBOOK_PATH ~/workspace/mihini/
-set INO_PATH ~/workspace/mihini/mihini/mihini.ino
+if test (uname) = 'Linux'
+  set ARDUINO_EXE_PATH /usr/local/bin/arduino
+  set BUILD_PATH ~/git/mihini/build
+  set SKETCHBOOK_PATH ~/git/mihini
+  set INO_PATH ~/git/mihini/mihini/mihini.ino
+else  # Darwin
+  set ARDUINO_EXE_PATH /Applications/Arduino.app/Contents/MacOS/Arduino
+  set BUILD_PATH ~/workspace/mihini/build
+  set SKETCHBOOK_PATH ~/workspace/mihini/
+  set INO_PATH ~/workspace/mihini/mihini/mihini.ino
+end
 
 eval $ARDUINO_EXE_PATH --verify --verbose $INO_PATH \
   --pref build.path=$BUILD_PATH \
