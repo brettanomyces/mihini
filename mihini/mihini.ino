ad694e66c5f76ea78bd9ca5d624502f34dd33ade
Brett <brett@Administrators-MacBook-Pro.local>
2015-06-06 16:57:36 +1200

    remove 4th led, simplify logic by combining onPeriod and onRemaining

--- mihini/mihini.ino
+++ mihini/mihini.ino
@@ -12,60 +12,60 @@ Relay relay;
 // on and off buttons are two pin, monmentary push buttons, connected 
 // from pin X to ground. When the button is pressed down,  digitalRead 
 // will return 0.
-// TODO tell relay if on is LOW or HIGH.
-int ON_PIN = 2;
-int OFF_PIN = 3;
-int RELAY_PIN = 4;
-int LED_0_PIN = 5;  // on if time remaining > 0
-int LED_1_PIN = 6;  // on if time remaining > 30 min
-int LED_2_PIN = 7;  // on if time remainint > 60 min
-int LED_3_PIN = 8;  // on if time remainint > 1 hour 30 min
+int RELAY_PIN = 7;
+uint8_t RELAY_OFF = HIGH;
+int OFF_PIN = 13;
+int ON_PIN = 12;
+int LED_0_PIN = 11;  // on if time remaining > 0
+int LED_1_PIN = 10;  // on if time remaining > ON_PERIOD_INCREMENT
+int LED_2_PIN = 9;  // on if time remainint > 2 * ON_PERIOD_INCREMENT
 
+// unsigned long ON_PERIOD_INCREMENT = 5000; // 5 seconds
 unsigned long ON_PERIOD_INCREMENT = 1800000; // 30 minutes
-unsigned long ON_PERIOD_MAX = 7200000; // 2 hours
-unsigned long WAIT_PERIOD = 2000;  // 2 seconds, time to wait for more button presses
-unsigned long onPeriod;
+unsigned long ON_PERIOD_MAX = 3 * ON_PERIOD_INCREMENT;
+unsigned long WAIT_PERIOD = 1000;  // 2 seconds, time to wait for more button presses
+unsigned long onRemaining;
 
 void setup() {                
-  onPeriod = 0;
+  Serial.begin(9600);
+  onRemaining = 0;
   onButton.setup(ON_PIN);
   offButton.setup(OFF_PIN);
-  relay.setup(RELAY_PIN);
+  relay.setup(RELAY_PIN, "", 0, RELAY_OFF);
+  pinMode(LED_0_PIN, OUTPUT);
   digitalWrite(LED_0_PIN, LOW);
+  pinMode(LED_1_PIN, OUTPUT);
   digitalWrite(LED_1_PIN, LOW);
+  pinMode(LED_2_PIN, OUTPUT);
   digitalWrite(LED_2_PIN, LOW);
-  digitalWrite(LED_3_PIN, LOW);
 }
 
 void loop() {
-  unsigned long onRemaining = onCountdown.remaining();
   displayOnRemaining(onRemaining);
   if (relay.isOn()){
-    if(onRemaining == 0 || offButton.wasPressed()){
+    onRemaining = onCountdown.remaining();
+    if(onRemaining == 0) {
+      relay.off();
+    } else if (offButton.wasPressed()){
+      onRemaining = 0;
       relay.off();
     }
   } else {  // relay is off 
     if (onButton.wasPressed()){
-      if (onPeriod < ON_PERIOD_MAX){
-        onPeriod += ON_PERIOD_INCREMENT;
+      if (onRemaining < ON_PERIOD_MAX){
+        onRemaining += ON_PERIOD_INCREMENT;
       }
       waitCountdown.reset(WAIT_PERIOD);
     }
-    if (waitCountdown.remaining() == 0){
+    if (waitCountdown.remaining() == 0 && onRemaining != 0){
       // turn on
       relay.on();
-      onCountdown.reset(onPeriod);
-      onPeriod = 0;  // reset on period
+      onCountdown.reset(onRemaining);
     }
   }
 }
 
 void displayOnRemaining(unsigned long _remaining){
-  if (_remaining > 3 * ON_PERIOD_INCREMENT) {
-    digitalWrite(LED_3_PIN, HIGH);
-  } else {
-    digitalWrite(LED_3_PIN, LOW);
-  }
   if (_remaining > 2 * ON_PERIOD_INCREMENT) {
     digitalWrite(LED_2_PIN, HIGH);
   } else {
